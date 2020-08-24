8ab8d32cb23cd2b759c1cf1ce7647474ee907c1a
wyx <wei00161>
2020-08-24 10:56:14 -0500

    parse BLAST result

--- main.cpp
+++ main.cpp
@@ -14,12 +14,14 @@ int main(int argc, char** argv) {
     result_file.open(file,ios::in);
     string line;
     size_t hit_num = 0;
-
+    string last = "#";
     while(getline(result_file,line)){
-        size_t pos = line.find("#");
-        if(pos != line.npos) hit_num++;
+        size_t pos_last = last.find("#");
+        size_t pos_now = line.find("#");
+        if(pos_now == line.npos && pos_last != last.npos) hit_num++;
+        else if(pos_last == last.npos && pos_now != line.npos) hit_num++;
     }
 
-    cout<<"no # line number: "<<hit_num<<". please divide primer_num"<<endl;
+    cout<<"violated primers: "<<hit_num/2<<endl;
     return 0;
 }
